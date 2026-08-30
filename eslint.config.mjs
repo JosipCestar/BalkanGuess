ee8511852798fe78d37a3b4306276c392636465a
JosipCestar <jcestar@mathos.hr>
2026-08-30 17:43:04 +0200

    Move SoundCloud attribution to fixed footer

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import js from "@eslint/js";
 import nextVitals from "eslint-config-next/core-web-vitals";
 
-const config = [js.configs.recommended, ...nextVitals, { ignores: [".next/**", ".next-dev/**", "node_modules/**", "prisma/generated/**"] }, { files: ["**/*.{ts,tsx}"], rules: { "no-undef": "off", "no-unused-vars": "off", "react-hooks/set-state-in-effect": "off" } }];
+const config = [js.configs.recommended, ...nextVitals, { ignores: [".next/**", ".next-dev/**", ".railway/**", "node_modules/**", "prisma/generated/**"] }, { files: ["**/*.{ts,tsx}"], rules: { "no-undef": "off", "no-unused-vars": "off", "react-hooks/set-state-in-effect": "off" } }];
 export default config;
